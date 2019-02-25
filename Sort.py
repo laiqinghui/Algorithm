2019035b10badbeb9fa02deb846ad4e30be9e3de
Qinghui <qinghui1994@gmail.com>
2019-02-25 17:50:13 +0800

    practiced quicksort partition routine

--- Sort.py
+++ Sort.py
@@ -43,3 +43,25 @@ def QuickSort(arr, left, right):
 arr = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 QuickSort(arr, 0, (len(arr)-1))
 print("Final: ", arr)
+
+
+def partition2(arr, left, right):
+    
+    pivot = math.floor((left + right)/2)
+    lastsmall = left
+    temp = arr[left]
+    arr[left] = arr[pivot]
+    arr[pivot] = temp
+
+    for i in range(left+1, right+1, 1):
+        if arr[i] < arr[left]:
+            temp = arr[i]
+            lastsmall += 1
+            arr[i] = arr[lastsmall]
+            arr[lastsmall] = temp
+
+    temp = arr[lastsmall]
+    arr[lastsmall] = arr[left]
+    arr[left] = temp
+
+    return lastsmall
